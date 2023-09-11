cc5f91c9fef47b082876e9b60d037abc11707286
CaseyGann <cmoden@gannacademy.org>
2023-09-11 10:28:02 -0400

    init

---
+++ src/App.java
@@ -0,0 +1,22 @@
+/** Task:
+ *  1. Write code that will take a single input from the user
+ *  2. Write code that will determine whether the input is divisible: by 2, or by 3, or by both 2 and 3, or by neither.
+ *     The code will then output a relevant message, for example:
+ *     "This number is divisible by 2, but not 3"
+ *     "This number is divisible by 3, but not 2"
+ *     "This number is divisible by 2 and 3"
+ *     "This number is divisible by neither 2 nor 3"
+ */
+
+
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+    }
+    
+}
